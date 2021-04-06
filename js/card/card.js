65fa5a9b4a599f609a7649af39bb98c095e0b435
Sasha Stepanova <chernishevskie@yandex.ru>
2021-04-05 23:29:29 -0300

    cпринт 7, рефакторинг кода (работа с классами)

---
+++ js/card/card.js
@@ -0,0 +1,37 @@
+export class Card {
+    constructor(name, link, openFullSizeImage) {
+        this._name = name;
+        this._link = link;
+        this._openFullSizeImage = openFullSizeImage;
+    }
+
+    createElementCard() {
+        const newItem = document
+        .querySelector('#template')
+        .content
+        .querySelector('.template__card')
+        .cloneNode(true); //клонируем элементы из  блока teamplate
+
+        this._templatePic = newItem.querySelector('.element__image');
+        this._templateTitle = newItem.querySelector('.element__quote');
+        this._templateTitle.textContent = this._name;
+        this._templatePic.alt = this._link;
+        this._templatePic.src = this._link;
+        this._deleteElementCard(newItem);
+        this._addLike(newItem);
+        this._openFullSizeImage(newItem);
+                
+        return newItem;
+    }
+
+    _deleteElementCard(element) {  /// метод удаления карточек ///
+        const deleteButton = element.querySelector('.element__trash');
+        deleteButton.addEventListener('click', () => element.remove());
+    }
+    
+    _addLike(element) {  /// метод для like-button ///
+        const likeButtomTemplate = element.querySelector('.element__button-like');
+        likeButtomTemplate.addEventListener('click', () => likeButtomTemplate.classList.toggle('element__button-like_active'));
+    }
+}
+
