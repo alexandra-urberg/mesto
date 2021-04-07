7e62de599d486e4130c3880a6a95c0c0d5c4f038
Sasha Stepanova <chernishevskie@yandex.ru>
2021-04-07 15:34:02 -0300

    спринт 7: fix - (1)добавила метод openfullsizeimage

--- js/card/card.js
+++ js/card/card.js
@@ -1,9 +1,10 @@
 export class Card {
-    constructor(name, link, openFullSizeImage, template) {
+    constructor(name, link, showPopup, template) {
         this._name = name;
         this._link = link;
-        this._openFullSizeImage = openFullSizeImage;
+        this._showPopup = showPopup;
         this._template = template;
+
     }
 
     createElementCard() {
@@ -17,9 +18,9 @@ export class Card {
         this._templateTitle.textContent = this._name;
         this._templatePic.alt = this._link;
         this._templatePic.src = this._link;
+        this._templatePic.addEventListener('click', () => this._openFullSizeImage(this._templatePic, this._templateTitle));
         this._deleteElementCard(newItem);
         this._addLike(newItem);
-        this._openFullSizeImage(newItem);
                 
         return newItem;
     }
@@ -32,6 +33,17 @@ export class Card {
     _addLike(element) {  /// метод для like-button ///
         const likeButtomTemplate = element.querySelector('.element__button-like');
         likeButtomTemplate.addEventListener('click', () => likeButtomTemplate.classList.toggle('element__button-like_active'));
+    }
+    
+    _openFullSizeImage(link, title) {
+        const imagePopup = document.querySelector('#image'); // блок image (увеличение фотографий)
+        const image = imagePopup.querySelector('.image'); // увеличенная фотография
+        const imageTitle = imagePopup.querySelector('.image-tittle'); // подпись фотографии
+
+        image.alt = title.textContent;
+        image.src = link.src;
+        imageTitle.textContent = title.textContent;
+        this._showPopup(imagePopup);
     }
 }
 
