0f72edf49be13ea6eab7784bd7c957725e077722
Sasha Stepanova <chernishevskie@yandex.ru>
2021-05-19 00:33:09 -0300

    refactor: спринт 9 - снимаю слушателей после закрытия попапов и обновила код

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -12,19 +12,21 @@ export default class Popup {
     close() { // метод закрытия popup окна 
         this._popup.classList.remove('popup_is-opened');
         document.removeEventListener('keydown', this._closeEscEvent);
+
+        this._removeEventListeners();
     }
 
     _handleEscClose = (event) => { /// метод закрытие popup по нажатию кнопки Esc (почему нужно навешивать его на открытие и закрытие)
-        if(event.key === "Escape") {
-            this.close();
-        }
-   }
+        if(event.key === "Escape") this.close();
+    }
 
     setEventListeners() {//метод для закртия popup по кнопке и overlay 
         this._popup.addEventListener('click', (evt) => {
-            if(evt.target.classList.contains('popup_is-opened') || evt.target.classList.contains('popup__close-button')) {
-                this.close();
-            } 
+            if(evt.target.classList.contains('popup_is-opened') || evt.target.classList.contains('popup__close-button')) this.close();
         })  
+    }
+    _removeEventListeners() { //метод для снятия слушателей после закртия попапов
+        this._popup.removeEventListener('click', this.setEventListeners);
+        this._popup.removeEventListener('keydown', this._closeEscEvent);
     }
 }
