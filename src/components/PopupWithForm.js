4eb2556214b2f504c0a7a907cb622257d420fdb3
Sasha Stepanova <chernishevskie@yandex.ru>
2021-05-19 00:31:36 -0300

    feat: спринт 9 - добавила apploadInformation, чтобы менялалась строчка на кнопке при загрузки картинок/информации

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,6 +5,8 @@ export default class PopupWithForm extends Popup {
         super(popupSelector);
         this._submithFormHandler = submithFormHandler; //ссылка на обработчик сабмита формы
         this._form = this._popup.querySelector('.popup__form-container'); //ссылка на форму определенного попапа
+        this._sbmtButton = this._form.querySelector('.popup__save-button'); //кнопка сабмита/сохранения/подтверждения в любом попапе
+        this._button = this._sbmtButton.textContent;
     }
 
     _getInputValues() {//метод получения данных из input 
@@ -15,10 +17,16 @@ export default class PopupWithForm extends Popup {
         return this._formValues;
     }
 
+    apploadInformation(isLoading) { //метод изменения текста на кнопках "создать/сохранить" при загрузке фотографий и информации
+        if(isLoading) this._sbmtButton.textContent = 'Сохранение...';
+        else this._sbmtButton.textContent = this._button;
+    }
+
     setEventListeners() { // теперь еще добавляет обработчик сабмита формы.
         super.setEventListeners();
         this._form.addEventListener('submit', (evt) => {
             evt.preventDefault();
+            this.apploadInformation(true);
             this._submithFormHandler(this._getInputValues());
         });
     }
