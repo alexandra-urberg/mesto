631000700fb6de22e6759fafd1b7165b75f9b917
Sasha Stepanova <chernishevskie@yandex.ru>
2021-05-19 00:22:09 -0300

    feat: спринт 9 - добавила метод меняющий avatar и добавила avatar в старые методы

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,18 +1,25 @@
 export default class UserInfo {
-    constructor({name, job}) {
-        this._elementname = document.querySelector(name);
-        this._elementinformation = document.querySelector(job);
+    constructor({name, about, avatar}) {
+        this._name = document.querySelector(name);
+        this._information = document.querySelector(about);
+        this._avatar = document.querySelector(avatar);
     }
 
     getUserInfo() { //метод получает информацию о пользователе
         return {
-            name: this._elementname.textContent,
-            job: this._elementinformation.textContent
+            name: this._name.textContent,
+            about: this._information.textContent,
+            avatar: this._avatar.src
         }
     }
 
     setUserInfo(data) { //метод принимает новые данные пользователя и добавляет их на страницу
-        this._elementname.textContent = data.name;
-        this._elementinformation.textContent = data.job;
+        this._name.textContent = data.name;
+        this._information.textContent = data.about;
+        this._avatar.style.backgroundImage = `url(${data.avatar})`;
+    }
+
+    changeAvatar({avatar}) {
+        this._avatar.style.backgroundImage = `url(${avatar})`;
     }
 }
