ac6a5fe3e26114e5b933f4fde2035a3cc706fb9a
Sasha Stepanova <chernishevskie@yandex.ru>
2021-05-21 00:28:04 -0300

    refactor: спринт 9(1) - рефактор кода удаления карточки и  добвила  Promise.all для для получения исходных данных и их синхронной обработки

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,127 +1,155 @@
-import './index.css'
-import Api from '../components/Api.js'
-import { Card } from '../components/Card.js'
-import { FormValidator } from '../components/Formvalidator.js'
-import Section from '../components/Section.js'
-import PopupWithImage from '../components/PopupWithImage.js'
-import PopupWithForm from '../components/PopupWithForm.js'
-import UserInfo from '../components/UserInfo.js'
-import PopupWithSubmith from '../components/PopupWithSubmit.js'
-import { validSelectors, openPopupProfileBtn, popupProfileBlock, nameInput,
-    jobInput, popupForm, openPopupImageBtn, popupImage, template, popupImageForm,
-    popupImageContainer, imagePopup, popupEditAvatar, openButtonPopupAatar, popupDeleteCard, popupDeleteButton } 
-from '../utils/utilites.js' 
-
+import "./index.css";
+import Api from "../components/Api.js";
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/Formvalidator.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import PopupWithSubmith from "../components/PopupWithSubmit.js";
+import {
+  validSelectors,
+  openPopupProfileBtn,
+  popupProfileBlock,
+  nameInput,
+  jobInput,
+  popupForm,
+  openPopupImageBtn,
+  popupImage,
+  template,
+  popupImageForm,
+  popupImageContainer,
+  imagePopup,
+  popupEditAvatar,
+  openButtonPopupAatar,
+  popupAvatarForm,
+  popupDeleteCard,
+} from "../utils/utilites.js";
 // Вызываем класс UserInfo
-const userInformation = new UserInfo({name: '.profile__name', about: '.profile__job', avatar: '.profile__avatar'});
-
+const userInformation = new UserInfo({
+  name: ".profile__name",
+  about: ".profile__job",
+  avatar: ".profile__avatar",
+});
 //Вызываем классы PopupWithImage и PopupWithForm для попап popupProfile и popupImage
 const popupOnlyImage = new PopupWithImage(imagePopup);
-const deleteCard = new PopupWithSubmith(popupDeleteCard, handleFormSubmit);
-
 // Вызывыем класс FormValidator с параметрами форм для popupProfile и popupImage ///
-const validNewCards = new FormValidator(validSelectors, popupImageForm); 
+const validNewCards = new FormValidator(validSelectors, popupImageForm);
 const validProfileBlock = new FormValidator(validSelectors, popupForm);
-const validAvatarPopup = new FormValidator(validSelectors, popupEditAvatar);
+const validAvatarPopup = new FormValidator(validSelectors, popupAvatarForm);
+// переменная в которую будем сохранять id user для удаления карточке
+let myId;
 
 const api = new Api({
-    url: 'https://mesto.nomoreparties.co/v1/cohort-24',
-    headers: {
-      authorization: '7dfaf4c9-82d9-4096-b562-ec8631a23ab5',
-      'Content-Type': 'application/json'
-    }
+  url: "https://mesto.nomoreparties.co/v1/cohort-24",
+  headers: {
+    authorization: "7dfaf4c9-82d9-4096-b562-ec8631a23ab5",
+    "Content-Type": "application/json",
+  },
 });
 
-let myId; // переменная в которую будем сохранять id user для удаления карточке 
-api.getPersonalInformation() //втаскиваем игформацию о user с сервера 
-.then(user => {
-    myId = user._id; // передавем id пользователя 
-    userInformation.setUserInfo(user);
-})
-.catch(error => console.log(error));
+const card = new Section((item) => {
+  const image = createCard(item);
+  card.addItem(image);
+}, popupImageContainer);
 
-api.getInitialCards() //вытаскиваем карточки с сервера
-.then(initialCards => {
+Promise.all([api.getPersonalInformation(), api.getInitialCards()])
+.then(([userData, initialCards]) => {
+    myId = userData;
+    userInformation.setUserInfo(myId);
     card.renderItems(initialCards);
 })
-.catch(error => console.log(error));
+.catch((error) => console.log(error));
 
-const formPopupProfile = new PopupWithForm({//изменение информации пользователя (добавление ее на сервер)
-    popupSelector: popupProfileBlock, 
-    submithFormHandler: (userData) => {
-        api.editPersonalProfile(userData)
-        .then(data => {
-            userInformation.setUserInfo(data);
-            formPopupProfile.close();
-        })
-        .catch(error => console.log(error))
-        .finally(() => formPopupProfile.apploadInformation(false));
-    }
+//изменение информации пользователя (добавление ее на сервер)
+const formPopupProfile = new PopupWithForm({
+  popupSelector: popupProfileBlock,
+  submithFormHandler: (userData) => {
+    api
+      .editPersonalProfile(userData)
+      .then((data) => {
+        userInformation.setUserInfo(data);
+        formPopupProfile.close();
+      })
+      .catch((error) => console.log(error))
+      .finally(() => formPopupProfile.updateButtonText(false));
+  },
+});
+//изменяем аватар пользователя(добавляем его на сервер)
+const formPopupAvatar = new PopupWithForm({
+  popupSelector: popupEditAvatar,
+  submithFormHandler: ({ avatar }) => {
+    api
+      .editAvatar(avatar)
+      .then((data) => {
+        userInformation.changeAvatar(data);
+        formPopupAvatar.close();
+      })
+      .catch((error) => console.log(error))
+      .finally(() => formPopupProfile.updateButtonText(false));
+  },
 });
 
-const formPopupAvatar = new PopupWithForm({//изменяем аватар пользователя(добавляем его на сервер)
-    popupSelector: popupEditAvatar,
-    submithFormHandler: ({avatar}) => {
-        api.editAvatar(avatar)
-        .then(data => {
-            userInformation.changeAvatar(data);
-            formPopupAvatar.close();
-        })
-        .catch(error => console.log(error))
-        .finally(() => formPopupProfile.apploadInformation(false));
-    }
-});
+function createCard(data) {
+  return new Card(
+    data,
+    myId,
+    template,
+    popupOnlyImage,
+    addLikes,
+    rejectLike,
+    onDeleteBtnClick
+  ).createElementCard();
+}
 
-function  createCard(data) {
-    return new Card(data, myId, template, popupOnlyImage, addLikes, rejectLike, handleFormSubmit, popupDeleteButton).createElementCard();
-};
-
-function addLikes(id, item) {//функция добавляет лайк
-    api.addLike(id)
-    .then(data => {
+//функция добавляет лайк
+function addLikes(id, item) {
+  api
+    .addLike(id)
+    .then((data) => {
       item.countLike(data);
     })
     .catch((err) => console.log(err));
 }
-  
-function rejectLike(id, item) {//функиця убирает лайк
-    api.deleteLike(id)
-    .then(data => {
-        item.countLike(data);
+
+//функиця убирает лайк
+function rejectLike(id, item) {
+  api
+    .deleteLike(id)
+    .then((data) => {
+      item.countLike(data);
     })
     .catch((err) => console.log(err));
 }
 
-const card = new Section((item) => {
-      const image = createCard(item);
-      card.addItem(image);
-    },
-    popupImageContainer
-);
+//добавляем карточку на сервер
+const formPopupImage = new PopupWithForm({
+  popupSelector: popupImage,
+  submithFormHandler: (data) => {
+    api
+      .addNewCard(data)
+      .then((data) => {
+        card.addItem(createCard(data));
+        formPopupImage.close();
+      })
+      .catch((error) => console.log(error))
+      .finally(() => formPopupImage.updateButtonText(false));
+  },
+});
 
-const formPopupImage = new PopupWithForm({ //добавляем карточку на сервер
-    popupSelector: popupImage,
-    submithFormHandler: (data) => {
-        api.addNewCard(data)
-        .then((data) =>{ 
-            card.addItem(createCard(data));
-            formPopupImage.close();
-        })
-        .catch(error => console.log(error))
-        .finally(() => formPopupImage.apploadInformation(false));
-    }
-})
-
-function handleFormSubmit(id,card) { //удаление карточки с сервера
-    api.deleteCard(id)
+//удаление карточки с сервера
+const onDeleteBtnClick = (id, card) => deleteCard.open(id, card); //передаем в card (сразу открывает попап удаления карточки)
+const deleteCard = new PopupWithSubmith(popupDeleteCard, (id, card) => {
+  api
+    .deleteCard(id)
     .then(() => deleteCard.close())
-    .catch(err => console.log(err))
-    .finally(() => setTimeout(card.remove(), 10000))
-}
+    .catch((err) => console.log(err))
+    .finally(() => setTimeout(card.remove(), 10000));
+});
 
 //проверяем валидность форм
 validNewCards.enableValidation();
-validProfileBlock.enableValidation(); 
+validProfileBlock.enableValidation();
 validAvatarPopup.enableValidation();
 
 //Обработчики событий
@@ -132,23 +160,23 @@ formPopupImage.setEventListeners();
 deleteCard.setEventListeners();
 
 /// Обработчкики закрытия/открытия Popup-ов ///
-openPopupProfileBtn.addEventListener('click', () => {
-    formPopupProfile.open();
-    validProfileBlock.removeErrors(); //удаление выявленных ошибок при закрытие popup через esc, overlay и кнопку "крестик"
-    validProfileBlock.disableSubmitButton(); //кнопка submit в состояние disabled после обнуления 
-    const userInfoCollect = userInformation.getUserInfo();
-    nameInput.value = userInfoCollect.name; 
-    jobInput.value = userInfoCollect.job;
+openPopupProfileBtn.addEventListener("click", () => {
+  formPopupProfile.open();
+  validProfileBlock.removeErrors(); //удаление выявленных ошибок при закрытие popup через esc, overlay и кнопку "крестик"
+  validProfileBlock.disableSubmitButton(); //кнопка submit в состояние disabled после обнуления
+  const userInfoCollect = userInformation.getUserInfo();
+  nameInput.value = userInfoCollect.name;
+  jobInput.value = userInfoCollect.about;
 });
 
-openButtonPopupAatar.addEventListener('click', () => {
-    formPopupAvatar.open();
-    validAvatarPopup.removeErrors();//удаление выявленных ошибок при закрытие popup через esc, overlay и кнопку "крестик"
-    validAvatarPopup.disableSubmitButton();//кнопка submit в состояние disabled после обнуления
+openButtonPopupAatar.addEventListener("click", () => {
+  formPopupAvatar.open();
+  validAvatarPopup.disableSubmitButton(); //кнопка submit в состояние disabled после обнуления
+  validAvatarPopup.removeErrors(); //удаление выявленных ошибок при закрытие popup через esc, overlay и кнопку "крестик"
 });
 
-openPopupImageBtn.addEventListener('click', () => {
-    formPopupImage.open();
-    validNewCards.disableSubmitButton(); //кнопка submit в состояние disabled после обнуления 
-    validNewCards.removeErrors(); //удаление выявленных ошибок при закрытие popup через esc, overlay и кнопку "крестик"
-}); 
+openPopupImageBtn.addEventListener("click", () => {
+  formPopupImage.open();
+  validNewCards.removeErrors(); //удаление выявленных ошибок при закрытие popup через esc, overlay и кнопку "крестик"
+  validNewCards.disableSubmitButton(); //кнопка submit в состояние disabled после обнуления
+});
